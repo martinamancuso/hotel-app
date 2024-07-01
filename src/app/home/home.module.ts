a490989ea89fe93757d6632ed5772208d0d1b68a
martinamancuso <martinamancuso24@gmail.com>
2024-07-01 15:55:51 +0200

    feat: build the navigation with "home", "create a new reservation" and "view the reservation list" pages

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-
-
+import { RouterLink } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, RouterLink],
 })
-export class HomeModule { }
+export class HomeModule {}
